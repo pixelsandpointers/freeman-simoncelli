3f327726d5880f650e1a33d33a25b0448c14336e
Benjamin Beilharz <benjamin.beilharz@pm.me>
2024-08-02 10:41:55 +0200

    added coarse-to-fine option and documentation

--- scenestats/helpers.py
+++ scenestats/helpers.py
@@ -7,34 +7,62 @@ import torchvision as tv
 
 from plenoptic.tools.signal import raised_cosine
 
-from .config import Config
-
 __all__ = [
     'GaussianPyramid',
     'compute_window',
     'compute_log_window'
 ]
 
+
 class GaussianPyramid(nn.Module):
     def __init__(self,
                  kernel_size: list[int] = [5, 5],
                  sigma: list[float] = [0.1, 2.0],
                  n_levels: int = 3):
+        """
+        A PyTorch implementation of the Gaussian Pyramid.
+
+        :param kernel_size: The kernel size of the Gaussian blur.
+        :param sigma: Standard deviation of the Gaussian blur.
+        :param n_levels: How many levels/layers to compute of the image.
+        """
         super(GaussianPyramid, self).__init__()
         self.n_levels = n_levels
         self.sigma = sigma
         self.gauss = tv.transforms.GaussianBlur(kernel_size=kernel_size, sigma=sigma)
+        # nn.Upsample is deprecated, so we use interpolate
         self.upsample = nn.functional.interpolate
 
     def downsample(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Downsamples the input tensor.
+        :param x: Input tensor, should be in shape batch x channels x height x width.
+        :return: The downsampled tensor
+        """
         assert x.ndim == 4, f'Tensor should have 4 dimensions but has {x.ndim}.'
         return x[:, :, ::2, ::2]
 
     def clamp(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Shifts the values by 128, to ensure that we don't have any negative values,
+        then clamps the values into RGB range of [0, 255].
+        :param x: Input tensor, should be in shape batch x channels x height x width.
+        :return: The clamped tensor.
+        """
         x += 128
         return torch.clamp(x, 0, 255)
 
-    def forward(self, img: torch.Tensor, max_levels: int | None = None) -> list[torch.Tensor]:
+    def forward(self,
+                img: torch.Tensor,
+                max_levels: int | None = None,
+                order_coarse_to_fine: bool = False) -> list[torch.Tensor]:
+        """
+        Forward pass of the Gaussian Pyramid.
+        :param img: input images, should be in shape batch x channels x height x width.
+        :param max_levels: if specified, number of layers to compute of the image, else falls back to class attribute.
+        :param order_coarse_to_fine: if true, layer order in list will be coarse to fine.
+        :return: a list of tensors with their respective scale
+        """
         max_levels = max_levels or self.n_levels
         current = img
         pyr = []
@@ -42,19 +70,20 @@ class GaussianPyramid(nn.Module):
             filtered = self.gauss(current)
             down = self.downsample(filtered)
             up = self.upsample(down, size=current.size()[2:])
-            diff = current-up
+            diff = current - up
             pyr.append(self.clamp(diff))
             current = down
-        return pyr
 
-
+        return pyr[::-1] if order_coarse_to_fine else pyr
 
 
 def compute_r():
     ...
 
+
 def compute_angle():
     ...
+
 
 def compute_window(center: int,
                    width: int,
@@ -78,13 +107,13 @@ def compute_window(center: int,
     :return: range of evaluated function and evaluation
     """
     t_width = t_width or [1, 1]
-    interval = interval or [center-width/2-t_width[0], center+width/2+t_width[1]]
+    interval = interval or [center - width / 2 - t_width[0], center + width / 2 + t_width[1]]
     min = min or 0
     max = max or 1
 
     minmax = abs(interval[-1] - interval[0])
-    X1, Y1 = raised_cosine(t_width[0], center - width/2 - t_width[0]/2, (min, max))
-    X2, Y2 = raised_cosine(t_width[1], center - width/2 - t_width[1]/2, (min, max))
+    X1, Y1 = raised_cosine(t_width[0], center - width / 2 - t_width[0] / 2, (min, max))
+    X2, Y2 = raised_cosine(t_width[1], center - width / 2 - t_width[1] / 2, (min, max))
     Y2 = -Y2 + (1 + min)
     x = np.concatenate((X1, X2))
     y = np.concatenate((Y1, Y2))
@@ -138,11 +167,11 @@ def compute_log_window(center: int,
     :return: range of evaluated function and evaluation
     """
     t_width = t_width or [1, 1]
-    interval = interval or [center-width/2-t_width[0], center+width/2+t_width[1]]
+    interval = interval or [center - width / 2 - t_width[0], center + width / 2 + t_width[1]]
 
     minmax = abs(interval[-1] - interval[0])
-    X1, Y1 = raised_cosine(t_width[0], center - width/2 - t_width[0]/2)
-    X2, Y2 = raised_cosine(t_width[1], center - width/2 - t_width[1]/2)
+    X1, Y1 = raised_cosine(t_width[0], center - width / 2 - t_width[0] / 2)
+    X2, Y2 = raised_cosine(t_width[1], center - width / 2 - t_width[1] / 2)
     Y2 = -Y2 + 1
     zero_indices = np.where(Y2 == 0)[0]
     substitute = Y2[-len(zero_indices)]
